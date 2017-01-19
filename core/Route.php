279841f4732fdcc47312aeb83c5fd5f61a7d2bf1
Jorgito <Jorgito@JORGITO-NB.home>
2017-01-19 20:24:27 -0200

    Routes: 19/01/2017

--- core/Route.php
+++ core/Route.php
@@ -7,8 +7,17 @@ class Route {
     private $routes;
     
     public function __construct(array $routes) {
-        $this->routes = $routes;
+        $this->setRoutes($routes);
         $this->run();
+    }
+    
+    private function setRoutes($routes) {
+        foreach ($routes as $route) {
+            $explode = explode('@', $route[1]);
+            $r = [$route[0], $explode[0], $explode[1]];
+            $newRoutes[] = $r;
+        }
+        $this->routes = $newRoutes;
     }
 
     private function getUrl() {
@@ -24,12 +33,24 @@ class Route {
             for ($index = 0; $index < count($routeArray); $index++) {
                 if ((strpos($routeArray[$index], "{") !== false) && (count($urlArray) == count($routeArray))) {
                     $routeArray[$index] = $urlArray[$index];
+                    $param[] = $urlArray[$index];
                 }
                 $route[0] = implode($routeArray, '/');
             }
+            
             if ($url == $route[0]) {
-                
+                $encontrada = true;
+                $controller = $route[1];
+                $acao = $route[2];
+                echo $route[0].'<br>';
+                echo $route[1].'<br>';
+                echo $route[2].'<br>';
+                echo $param[0].'<br>';
+                break;
             }
+        }
+        if ($encontrada) {
+            
         }
     }
     
